1860b5e258c13a8cc9a706ac7c393b511d9683c2
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-03-16 09:39:14 -0600

    ejercicios html

---
+++ formulario.js
@@ -0,0 +1,18 @@
+var datosValidacion = [];
+var nombre = toLowerCase(prompt=("Ingresa nombre completo"));
+var telefono = parseInt(prompt("Ingresa tu numero celular a 10 digitos"));
+var email = prompt("Ingresa tu correo electronico");
+var contra = prompt("Agrega una contraseña");
+  var datosCompletos = {
+    nombre: nombre,
+    telefono: telefono,
+    email: email,
+    contra: contra,
+
+//for(i=0; i<datosCompletos.length ;i++){
+
+  //}
+
+    //datosValidacion.push(datosCompletos);
+    //document.write(datosValidacion)
+}
